ab57d3bcc17af03cd6a4e37ab0d86b7f9c22d3a2
nikshcherbakov <shcerbackow@yandex.ru>
2021-09-28 22:55:28 +0300

    Changed pom files

--- client/src/main/java/com/nikshcherbakov/myapi/MyApiClient.java
+++ client/src/main/java/com/nikshcherbakov/myapi/MyApiClient.java
@@ -5,7 +5,7 @@ import com.nikshcherbakov.dto.ObjectDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "MyApiClient", url = "http://localhost:8080")
+@FeignClient(name = "MyApiClient", url = "http://localhost:8080", path = "/api")
 public interface MyApiClient extends MyApi {
 
     @GetMapping(value = "/object")
--- server/src/main/java/com/nikshcherbakov/server/rest/ServerRestController.java
+++ server/src/main/java/com/nikshcherbakov/server/rest/ServerRestController.java
@@ -2,15 +2,17 @@ package com.nikshcherbakov.server.rest;
 
 import com.nikshcherbakov.MyApi;
 import com.nikshcherbakov.dto.ObjectDto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
 public class ServerRestController implements MyApi {
 
-    @Override
+    @GetMapping("/object")
     public ObjectDto getObject() {
         return new ObjectDto("Example name", "Example value");
     }
+
 }
